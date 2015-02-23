6436e0bd7910cc22364f9d373b2f30030c1d8897
sb12 <sb12@users.noreply.github.com>
2015-02-23 11:25:25 +0100

    fix typo

--- briefkastenkarte.js
+++ briefkastenkarte.js
@@ -36,7 +36,7 @@ var post_box = new L.OverPassLayer({
 			var popup = '<div class="wrapper"><div class="table"><div class="row_pp header green"><div class="cell">Briefkasten</div><div class="cell"></div></div>';
 			if ((!e.tags.collection_times) && (!e.tags.operator) && (!e.tags.brand) && (!e.tags.ref)) {popup = popup + '<div class="row_pp"><div class="cell">Keine weiteren Informationen verfügbar.</div></div>'};
 			
-			if (e.tags.collection_times) {popup = popup + '<div class="row_pp"><div class="cell"><b>Leerungsszeiten:</b></div><div class="cell">' + e.tags.collection_times.replace("Su", "So") + '</div></div>'};
+			if (e.tags.collection_times) {popup = popup + '<div class="row_pp"><div class="cell"><b>Leerungszeiten:</b></div><div class="cell">' + e.tags.collection_times.replace("Su", "So") + '</div></div>'};
 			if (e.tags.operator) {popup = popup + '<div class="row_pp"><div class="cell"><b>Betreiber:</b></div><div class="cell">' + e.tags.operator + '</div></div>'};
 			if (e.tags.brand) {popup = popup + '<div class="row_pp"><div class="cell"><b>Marke:</b></div><div class="cell">' + e.tags.brand + '</div></div>'};
 			if (e.tags.ref) {popup = popup + '<div class="row_pp"><div class="cell"><b>Standort:</b></div><div class="cell">' + e.tags.ref + '</div></div>'};
